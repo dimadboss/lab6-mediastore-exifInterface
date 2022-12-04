4b1c9fba5add2ae73ca939cb5f706a8ada65a838
D Drozdov <ds-drozdov@yandex.ru>
2022-12-04 19:54:51 +0300

    change geo type to double

--- app/src/main/java/com/example/lab6_mediastore_exifinterface/data/ExifData.kt
+++ app/src/main/java/com/example/lab6_mediastore_exifinterface/data/ExifData.kt
@@ -6,18 +6,18 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 open class ExifData(
-    val date: String? = null,
+    var date: String? = null,
     val latitude: String? = null,
     val latitudeRef: String? = null,
     val longitude: String? = null,
     val longitudeRef: String? = null,
-    val device: String? = null,
-    val model: String? = null,
+    var device: String? = null,
+    var model: String? = null,
 ) : Parcelable
 
 data class Geo(
-    val latitude: Float,
-    val longitude: Float,
+    val latitude: Double,
+    val longitude: Double,
 )
 
 fun Geo?.toString(): String {
@@ -52,22 +52,22 @@ fun ExifData?.toStringPretty(): String {
     """.trimIndent()
 }
 
-private fun convertToDegree(stringDMS: String): Float {
+private fun convertToDegree(stringDMS: String): Double {
     val dms = stringDMS.split(",")
 
     val floatD = dms[0].split("/")
-    val d0 = floatD[0].toFloat()
-    val d1 = floatD[1].toFloat()
+    val d0 = floatD[0].toDouble()
+    val d1 = floatD[1].toDouble()
     val d = d0 / d1
 
     val floatM = dms[1].split("/")
-    val m0 = floatM[0].toFloat()
-    val m1 = floatM[1].toFloat()
+    val m0 = floatM[0].toDouble()
+    val m1 = floatM[1].toDouble()
     val m = m0 / m1
 
     val floatS = dms[2].split("/")
-    val s0 = floatS[0].toFloat()
-    val s1 = floatS[1].toFloat()
+    val s0 = floatS[0].toDouble()
+    val s1 = floatS[1].toDouble()
     val s = s0 / s1
 
     return d + m / 60 + s / 3600
